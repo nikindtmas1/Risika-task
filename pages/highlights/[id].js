cb6d203cf84e4f89b253d1579d72aad2ec03fdcb
Nikolai Nikolaev <niki_nik_74@abv.bg>
2021-12-15 19:29:44 +0200

    Added sort logic

--- pages/highlights/[id].js
+++ pages/highlights/[id].js
@@ -3,7 +3,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 
-import Tamplate from "@/components/Custom/Template";
+
 
 const host = "http://localhost:3000";
 
@@ -11,7 +11,9 @@ const host = "http://localhost:3000";
 const Highlights = () => {
     const router = useRouter();
     const { id } = router.query;
+  
     let arrayData = [];
+    let sortedData = [];
 
     const [companyData, setCompanyData] = React.useState(null);
 
@@ -23,25 +25,41 @@ const Highlights = () => {
             .then(({ data }) => { setCompanyData(data) }
             )
     }, [id]);
+ 
     if(companyData != undefined){
-        console.log(Object.keys(companyData));
-        console.log(Object.entries(companyData));
         arrayData = Object.entries(companyData);
+        sortedData = arrayData;
     }
 
-    let sortArray = arrayData.filter(x => x[1].classification === 'positive')
+    if(router.query.sort === 'negative'){
+       sortedData = arrayData.filter(x => x[1].classification === 'negative')
+    }else if(router.query.sort === 'positive'){
+      sortedData = arrayData.filter(x => x[1].classification === 'positive')
+    }else if(router.query.sort === 'neutral'){
+      sortedData = arrayData.filter(x => x[1].classification === 'neutral')
+    }else if(router.query.sort === 'weight'){
+       sortedData = arrayData.filter(x => x[1].classification === 'weight')
+    }
 
 
     return (
         <Layout>
             <div className="container">
-              
-            {arrayData.map(x =>  
+
+            <p>
+              <a className="btn btn-primary btn-lg" href={`?sort=negative`} role="button">Sort by negative</a>
+              <a className="btn btn-primary btn-lg" href={`?sort=positive`} role="button">Sort by positive</a>
+              <a className="btn btn-primary btn-lg" href={`?sort=neutral`} role="button">Sort by neutral</a>
+              <a className="btn btn-primary btn-lg" href={`?sort=weight`} role="button">Sort by weight</a>
+              <a className="btn btn-primary btn-lg" href={`/highlights/${id}`} role="button">Not Sort</a>
+            </p>
+
+            {sortedData.map(x =>  
             <div class="panel panel-default">
                 <div class="panel-heading">{x[0]}</div>
-                {Object.keys(x[1]).map(x => 
+                {Object.keys(x[1]).forEach(key => 
                     
-                <div class="panel-body">{x}</div>
+                <div class="panel-body">{(x[1][key])}</div>
                     )}
             </div>)}
             
